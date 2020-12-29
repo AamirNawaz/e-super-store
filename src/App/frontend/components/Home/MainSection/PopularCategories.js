a62f0e474c5fbee6f120e44838750b7dd21bf1ea
asifUser1123 <asif.khan45201@gmail.com>
2020-12-29 22:26:28 +0500

    home page image fixing

--- src/App/frontend/components/Home/MainSection/PopularCategories.js
+++ src/App/frontend/components/Home/MainSection/PopularCategories.js
@@ -1,5 +1,7 @@
 import React from 'react'
-
+import item1 from "../../../../assets/images/items/1.jpg";
+import item2 from '../../../../assets/images/items/2.jpg'
+import item3 from '../../../../assets/images/items/3.jpg'
 export default function PopularCategories() {
     return (
         <React.Fragment>
@@ -11,21 +13,21 @@ export default function PopularCategories() {
                             <h6 className="card-title">Men clothing</h6>
                             <a href="/" className="btn btn-secondary btn-sm"> Source now </a>
                         </div>
-                        <img src='./image1' alt="item 1" height={80} className="img-bg" />
+                        <img src={item1} alt="item 1" height={80} className="img-bg" />
                     </div>
                     <div className="card-banner border-bottom">
                         <div className="py-3" style={{ width: '80%' }}>
                             <h6 className="card-title">Winter clothing </h6>
                             <a href="/" className="btn btn-secondary btn-sm"> Source now </a>
                         </div>
-                        <img src="./img2" alt="item 2" height={80} className="img-bg" />
+                        <img src={item2} alt="item 2" height={80} className="img-bg" />
                     </div>
                     <div className="card-banner border-bottom">
                         <div className="py-3" style={{ width: '80%' }}>
                             <h6 className="card-title">Home inventory</h6>
                             <a href="/" className="btn btn-secondary btn-sm"> Source now </a>
                         </div>
-                        <img src="./img3" alt="item 3" height={80} className="img-bg" />
+                        <img src={item3} alt="item 3" height={80} className="img-bg" />
                     </div>
                 </aside>
             </div> {/* col.// */}
--- src/App/frontend/components/Home/index.js
+++ src/App/frontend/components/Home/index.js
@@ -4,11 +4,9 @@ import MainSection from './MainSection/index'
 import DealSection from './DealSection/index'
 import ApparelSection from './ApparelSection/index'
 import ElectronicsSection from './ElectronicsSection/index'
-import Quotation from './Quotation/index'
 import RecommendedItems from './RecommendedItems/index'
-import ServiceSection from './ServiceSection/index'
-import RegionSection from './RegionSection/index'
 import Footer from '../../layouts/Footer'
+import SubscribeNewsLater from '../subscribeNewsLater';
 
 
 import bannerImg from '../../../assets/images/banners/ad-sm.png';
@@ -23,37 +21,12 @@ export default function index() {
                 <DealSection />
                 <ApparelSection />
                 <ElectronicsSection />
-                <Quotation />
                 <RecommendedItems />
-                <ServiceSection />
-                <RegionSection />
                 <article className="my-4">
                     <img src={bannerImg} alt="bannerIg" className="w-100" />
                 </article>
-
             </div>
-
-            {/* ========================= SECTION SUBSCRIBE  ========================= */}
-            <section className="section-subscribe padding-y-lg">
-                <div className="container">
-                    <p className="pb-2 text-center text-white">Delivering the latest product trends and industry news straight to your inbox</p>
-                    <div className="row justify-content-md-center">
-                        <div className="col-lg-5 col-md-6">
-                            <form className="form-row">
-                                <div className="col-md-8 col-7">
-                                    <input className="form-control border-0" placeholder="Your Email" type="email" />
-                                </div> {/* col.// */}
-                                <div className="col-md-4 col-5">
-                                    <button type="submit" className="btn btn-block btn-warning"> <i className="fa fa-envelope" /> Subscribe </button>
-                                </div> {/* col.// */}
-                            </form>
-                            <small className="form-text text-white-50">We’ll never share your email address with a third-party. </small>
-                        </div> {/* col-md-6.// */}
-                    </div>
-                </div>
-            </section>
-            {/* ========================= SECTION SUBSCRIBE END// ========================= */}
-
+            <SubscribeNewsLater />
             <Footer />
 
         </React.Fragment>
