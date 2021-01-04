c5329ac418152da9bc2fd33bc9ae123074956a49
Aamir nawaz <vuprogramers@gmail.com>
2021-01-05 00:45:58 +0500

    added cors packge in backend

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -12,7 +13,7 @@ var categoryRoutes = require('./routes/categoryRoutes')
 const { dbConnection } = require('./db/config');
 
 var app = express();
-
+app.use(cors())
 // db connection call
 dbConnection();
 
