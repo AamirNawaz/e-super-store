1c710c1e1eb17ef87571961a64a3b0ec5f1ac7af
Aamir nawaz <vuprogramers@gmail.com>
2021-01-14 18:52:08 +0500

    Dashboard sidebar Nav link active on click done, with showing name on Header of loggedin user as well

--- src/App/admin/components/NavTop.js
+++ src/App/admin/components/NavTop.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import jwtDecode from 'jwt-decode';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { userLogout } from '../../redux/reducer/Auth/authActions';
 function NavTop(props) {
+    const [userName, setUserName] = useState('');
+    useEffect(() => {
+        if (props.auth.authToken) {
+            const decode = jwtDecode(props.auth.authToken);
+            setUserName(decode.user.name);
+        }
+    }, [props.auth, userName]);
+
     return (
         <React.Fragment>
             <nav className="sb-topnav navbar navbar-expand navbar-dark bg-dark">
@@ -10,7 +19,7 @@ function NavTop(props) {
                 <button className="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="/"><i className="fas fa-bars" /></button>
 
                 {/* Navbar*/}
-                <div className="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0" style={{ color: 'white', marginRight: '-9px !important' }}>Admin</div>
+                <div className="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0" style={{ color: 'white', marginRight: '-9px !important' }}>{userName}</div>
                 <ul className="navbar-nav ml-auto ml-md-0 ">
                     <li className="nav-item dropdown">
                         <a className="nav-link dropdown-toggle" id="userDropdown" href="/" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i className="fas fa-user fa-fw" /></a>
@@ -26,9 +35,16 @@ function NavTop(props) {
         </React.Fragment>
     )
 }
+
+const mapStateToProps = (state) => {
+    return {
+        auth: state.auth
+    }
+}
+
 const mapDispatchToProps = (dispatch) => {
     return {
         logoutBtn: () => dispatch(userLogout())
     }
 }
-export default connect(null, mapDispatchToProps)(NavTop);
+export default connect(mapStateToProps, mapDispatchToProps)(NavTop);
