d1693723653e9a99da18cfdba5c239049706ffe6
Aamir nawaz <vuprogramers@gmail.com>
2021-01-19 18:37:13 +0500

    now pointing node js from  aws

--- frontend/src/App/AppConstant.js
+++ frontend/src/App/AppConstant.js
@@ -1,6 +1,14 @@
 // export const REACT_APP_ENV = 'Development';
 export const REACT_APP_ENV = 'production';
-export const API_END_POINT = 'https://e-super-store.herokuapp.com/api';
+// export const API_END_POINT = 'https://e-super-store.herokuapp.com/api';
+// export const DEV_API_END_POINT = 'http://localhost:9000/api';
+// export const NODE_IMAGES_PATH = 'https://e-super-store.herokuapp.com/productImages/';
+// export const DEV_NODE_IMAGES_PATH = 'http://localhost:9000/productImages/';
+
+
+export const API_END_POINT = 'http://13.233.149.126:9000/api';
 export const DEV_API_END_POINT = 'http://localhost:9000/api';
-export const NODE_IMAGES_PATH = 'https://e-super-store.herokuapp.com/productImages/';
+export const NODE_IMAGES_PATH = 'http://13.233.149.126:9000/productImages/';
 export const DEV_NODE_IMAGES_PATH = 'http://localhost:9000/productImages/';
+
+
