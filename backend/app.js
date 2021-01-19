b4bf6b13ed014a24a8d4ffb489a942df0c89af06
Aamir nawaz <vuprogramers@gmail.com>
2021-01-19 21:44:15 +0500

    setting react for node in aws

--- backend/app.js
+++ backend/app.js
@@ -27,8 +27,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 
+
+
+if (process.env.APP_ENV === 'production') {
+  app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+  });
+}
 app.use('/', indexRoute);
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
