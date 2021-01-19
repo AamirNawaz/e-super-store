5c04733056d7bd512cd6ba6e82aa3a503e0d40df
Aamir nawaz <vuprogramers@gmail.com>
2021-01-19 21:15:08 +0500

    now testing with full dns url of aws

--- frontend/src/App/AppConstant.js
+++ frontend/src/App/AppConstant.js
@@ -6,9 +6,9 @@ export const REACT_APP_ENV = 'production';
 // export const DEV_NODE_IMAGES_PATH = 'http://localhost:9000/productImages/';
 
 
-export const API_END_POINT = 'http://13.233.149.126:9000/api';
+export const API_END_POINT = 'http://ec2-13-233-149-126.ap-south-1.compute.amazonaws.com/api';
 export const DEV_API_END_POINT = 'http://localhost:9000/api';
-export const NODE_IMAGES_PATH = 'http://13.233.149.126:9000/productImages/';
+export const NODE_IMAGES_PATH = 'http://ec2-13-233-149-126.ap-south-1.compute.amazonaws.com/productImages/';
 export const DEV_NODE_IMAGES_PATH = 'http://localhost:9000/productImages/';
 
 
