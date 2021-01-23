1b4055f1ce375e037685940c29a5d6ad3f6cc8f8
Aamir nawaz <vuprogramers@gmail.com>
2021-01-23 15:56:30 +0500

    space removed

--- frontend/src/App/AppConstant.js
+++ frontend/src/App/AppConstant.js
@@ -1,5 +1,6 @@
 // export const REACT_APP_ENV = 'Development';
 export const REACT_APP_ENV = 'production';
+
 // export const API_END_POINT = 'https://e-super-store.herokuapp.com/api';
 // export const DEV_API_END_POINT = 'http://localhost:9000/api';
 // export const NODE_IMAGES_PATH = 'https://e-super-store.herokuapp.com/productImages/';
